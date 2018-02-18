0cba829daba3fe5cf05e46f0816b6b9fd4d63844
eal6gf <elum1234@gmail.com>
2018-02-17 21:32:03 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  state = {
+    hometown: '',
+    username: ''
+  }
+
+  handleHometown = (e) => {
+    this.setState({
+      hometown: e.target.value
+    })
+  }
+
+  handleUsername = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleSubmit}>
+          <input type='text' name='username' placeholder='username' onChange={this.handleUsername} ></input>
+          <input type='text' name='hometown' placeholder='hometown' onChange={this.handleHometown} ></input>
+
+          <input type='submit'></input>
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import manageUsers from './reducers/manageUsers';
 import App from './App';
-
+import { Provider } from 'react-redux'
 // we wrap store in a function for testing purposes
 export function configureStore(){
   return createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 };
 
 ReactDOM.render(
-  <App store={configureStore()} />,
+  <Provider>
+    <App store={configureStore()} />
+  </Provider>,
   document.getElementById('root')
 );
